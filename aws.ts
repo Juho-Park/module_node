8dfe961c2fb8dbdf70d390d7233909ff7402308e
zuo <c.jhpark12@gmail.com>
2025-06-30 21:14:39 +0900

    rename ENV varialbes

--- aws.ts
+++ aws.ts
@@ -11,8 +11,8 @@ const Region = 'ap-northeast-2'
 const s3 = new S3Client({
     region: Region,
     credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY!,
-        secretAccessKey: process.env.AWS_SECRET_KEY!
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!
     }
 });
 const Bucket = process.env.AWS_BUCKET
