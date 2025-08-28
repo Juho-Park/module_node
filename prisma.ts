a9fa7962073ce809d209833b0fce994794f008fc
zuo <c.jhpark12@gmail.com>
2025-08-28 21:03:02 +0900

    add comment

--- prisma.ts
+++ prisma.ts
@@ -4,6 +4,7 @@ const prisma = new PrismaClient({
     log: [{ emit: 'event', level: 'query' }, 'info', 'warn', 'error']
 })
 
+// for custom logging
 prisma.$on("query", (e) => {
     // SQL 명령어 (SELECT, UPDATE 등)
     const type = e.query.split(" ")[0];
