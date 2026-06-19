4f05fdd57476add6b1f0701996a860eeffbbc3e8
zuo <c.jhpark12@gmail.com>
2026-06-19 16:20:24 +0900

    move new Resend to send()

--- resend.ts
+++ resend.ts
@@ -1,13 +1,14 @@
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 export function send(to: string, subject: string, html: string) {
-  if (!process.env.RESEND_FROM) throw new Error('RESEND_FROM is not defined');
-  return resend.emails.send({
-    from: process.env.RESEND_FROM || '',
-    to,
-    subject,
-    html
-  });
+    if (!process.env.RESEND_API_KEY) throw new Error('RESEND_API_KEY is not defined');
+    if (!process.env.RESEND_FROM) throw new Error('RESEND_FROM is not defined');
+
+    const resend = new Resend(process.env.RESEND_API_KEY);
+    return resend.emails.send({
+        from: process.env.RESEND_FROM || '',
+        to,
+        subject,
+        html
+    });
 }
